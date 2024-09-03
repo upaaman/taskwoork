866894fe33403d81b3b27c529053d79ab899aa21
upaaman <upadhyayaman9685@gmail.com>
2024-09-03 21:32:20 +0530

    first commit

--- src/Pages/Hotels.jsx
+++ src/Pages/Hotels.jsx
@@ -7,7 +7,7 @@ const Hotels = () => {
                HOTELS
             </div>
             <div className="flex flex-col lg:flex-row  gap-10">
-                <img className="w-[450px]" src="../public/imageUsed.png" alt="" />
+                <img className="w-[450px]" src="https://t4.ftcdn.net/jpg/04/60/07/71/240_F_460077135_4MB32u4fUP5tKdqFTFTEdj0S9rjkaCFI.jpg" alt="" />
                 <p>Backend development is a crucial aspect of modern software engineering that focuses on the server side of web applications. Unlike frontend development, which deals with the visual and interactive aspects of a website, backend development is concerned with the behind-the-scenes processes that ensure the smooth operation, security, and efficiency of an application. This includes managing databases, ensuring server performance, implementing business logic, and handling user authentication. The backend is the backbone of any web application, making it an essential component in the development lifecycle.
 
                     At its core, backend development revolves around creating, managing, and optimizing server-side logic. This includes writing server-side code that interacts with databases, processes requests, and returns the appropriate responses to client-side applications. Backend developers work with various programming languages, frameworks, and tools to build robust and scalable applications. Popular programming languages used in backend development include Python, Java, Ruby, PHP, and JavaScript (specifically Node.js). Each of these languages has its strengths and is chosen based on the specific needs of the project.
